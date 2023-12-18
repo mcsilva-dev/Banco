898ccba0f526d708bac463267bb8b25b30a80a0b
Miguel <miguelcamposdasilva@gmail.com>
2023-12-18 15:51:00 -0300

    Implementando assert

--- client/client.py
+++ client/client.py
@@ -5,19 +5,14 @@ import re
 
 class Client(Bank):
     def __init__(self, name, document_number, date_of_birth, phone_number):
+        assert re.compile(r'(\d{3}).(\d{3}).(\d{3})\-(\d{2})').fullmatch(document_number), 'Documento inválido'
+        assert re.compile(r'(\d{4})\-(\d{2})\-(\d{2})').fullmatch(date_of_birth), 'Data de nascimento inválida'
+        assert re.compile(r'^(\+55).*$').fullmatch(phone_number), 'Formato de telefone inválido'
+        
         self.name = name
-        if re.compile(r'(\d{3}).(\d{3}).(\d{3})\-(\d{2})').fullmatch(document_number):
-            self.document_number = document_number
-        else:
-            raise ValueError('Documento inválido')
-        if re.compile(r'(\d{4})\-(\d{2})\-(\d{2})').fullmatch(date_of_birth):
-            self.date_of_birth = date_of_birth
-        else:
-            raise ValueError('Data de nascimento inválida')
-        if re.compile(r'^(\+55).*$').fullmatch(phone_number):
-            self.phone_number = phone_number
-        else:
-            raise ValueError("Formato de telefone inválido")
+        self.document_number = document_number
+        self.date_of_birth = date_of_birth
+        self.phone_number = phone_number
         
     def create_account(self):
         super().__init__(f'{datetime.now().day}/{datetime.now().month}/{datetime.now().year}')
